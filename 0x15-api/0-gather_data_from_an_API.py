125e91ffd26f94cbf7bda6bdd2bf725e955ecc43
Dinobi <architectstan01@gmail.com>
2023-05-22 11:32:10 -0700

    API setup

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,16 +5,16 @@ import sys
 if __name__ == "__main__":
 
     url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
-    req = requests.get(url).json()
-    name = req.get("name")
+    req = requests.get(url)
+    name = req.json().get("name")
 
     url_2 = 'https://jsonplaceholder.typicode.com/todos'
-    req2 = requests.get(url_2).json()
+    req2 = requests.get(url_2)
     task_num = 0
     filed_task = []
     completed_tasks = 0
 
-    for task in req2:
+    for task in req2.json():
         if task.get('userId') == int(sys.argv[1]):
             task_num += 1
         if task.get("completed_tasks") is True:
