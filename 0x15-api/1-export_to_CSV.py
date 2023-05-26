9750dbcb18793f9f765ce7814b9a17140a8b1b79
Dinobi <architectstan01@gmail.com>
2023-05-26 02:13:47 -0700

    Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Exports data in the CSV format """
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/users/' + sys.argv[1]
+    req = requests.get(url)
+    userName = req.json().get("username")
+
+    url2 = 'https://jsonplaceholder.typicode.com/todos'
+    req2 = requests.get(url2)
+
+    files = sys.argv[1] + '.csv'
+    line = []
+    for i in req2.json():
+        if i.get("userId") == int(sys.argv[1]):
+            line.append([i.get("userId"), userName, i.get("completed"), i.get("title")])
+
+    with open(files, 'w') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for r in line:
+            writer.writerow(r)
