23f3dfade96831260f2fc1211c4bb085c048e660
Dinobi <architectstan01@gmail.com>
2023-06-06 14:40:39 -0700

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,7 @@ A function that queries the API and returns the number of subscribers
 """
 import requests
 
-def def number_of_subscribers(subreddit):
+def number_of_subscribers(subreddit):
     """ Gets number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     req = requests.get(url, headers={'User-Agent': 'Dinobi 1.0'})
