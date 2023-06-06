4e48fcafed1b490ecb07b715d783f08eed46d488
Dinobi <architectstan01@gmail.com>
2023-06-06 14:37:24 -0700

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,6 +7,7 @@ import requests
 def def number_of_subscribers(subreddit):
     """ Gets number of subscribers"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    req = requests.get(url)
-    if req.json().get('data').get('subscribers')  is None:
+    req = requests.get(url, headers={'User-Agent': 'Dinobi 1.0'})
+    if req.status_code != 200:
         return 0
+    return req.json().get('data').get('subscribers')
